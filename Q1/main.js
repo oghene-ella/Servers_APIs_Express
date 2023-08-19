ff629a60fc790e85052a584f56c6047909fdea3d
oghene-ella <oghenekaro57@gmail.com>
2023-08-19 18:08:00 +0100

    worked on question 1

--- Q1/main.js
+++ Q1/main.js
@@ -1,45 +1,60 @@
-/* Import all the modules needed for Question 1 */
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
+/* Import all the modules needed */
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
 /* Create an Express Server */
 const expressServer = express();
 
 /* Create a port and hostname */
 const port = 2130;
-const hostname = 'localhost';
+const hostname = "localhost";
 
 /* Get the static files using path modules */
 const indexFile = path.join(__dirname, "static", "index.html");
 const errorFile = path.join(__dirname, "static", "error.html");
 
-/* Create a middleware that can get static files */
-expressServer.use(express.static("static"))
+/* Create a middleware that serves static files from the "static" directory */
+expressServer.use(express.static("static"));
 
-/* Get /index.html file */
-expressServer.get("/index.html", async (req, res) => {
-    const readIndex = await fs.readFile(indexFile);
-    res.status(200);
-    res.sendFile(readIndex);
-})
-
-/* Get /{random}.html file */
-expressServer.get("*", async (req, res) => {
+/* Route for handling requests */
+expressServer.get("/", async (req, res) => {
 	try {
-		const readFileError = await fs.readFile(errorFile);
-        res.status(404);
-        // fix this error [ its in buffer]
-        console.log(readFileError)
-		// res.sendFile(readError);
+		const readIndex = await fs.readFile(indexFile);
+		res.status(200);
+        res.send(readIndex);
 	} 
+    
     catch (error) {
-		console.log(error);
+		console.error(error);
+		res.status(500)
+        res.send("Internal Server Error");
 	}
 });
 
+/* Route for handling requests for other HTML files */
+expressServer.get("/:filename", async (req, res) => {
+	const requestedFile = req.params.filename;
 
-/* Add an Event Listener  */
+	if (requestedFile.endsWith(".html")) {
+		try {
+			res.status(400);
+			res.sendFile(errorFile);
+		} 
+        catch (error) {
+			console.error(error);
+			res.status(500);
+			res.send("Internal Server Error");
+		}
+	} 
+    
+    else {
+		res.status(404);
+		res.send("File Not Found");
+	}
+});
+
+/* Start the server */
 expressServer.listen(port, hostname, () => {
-    console.log(`Connecting to http://${hostname}:${port}`);
-})
+	console.log(`Server is running at http://${hostname}:${port}`);
+});
