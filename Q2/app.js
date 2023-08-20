a9895de0432c4040657a60dbfbce913631ad7100
Oghenekaro Emmanuella <oghenekaro57@gmail.com>
2023-08-20 09:00:14 +0100

    Update app.js

--- Q2/app.js
+++ Q2/app.js
@@ -11,9 +11,9 @@ const appServer = express();
 const port = 3040;
 const hostname = "localhost";
 
-/* Create a middleware that handles routing for /inventory */
+/* Create a middleware that handles routing for the home route '/' */
 appServer.get("/", (req, res) => {
-	res.end("Hello!, Welcome\nMake use of '/inventory' and api methods to route.  ");
+	res.end("Hello!, Welcome\nMake use of '/inventory' and api methods to route. ");
 });
 
 /* Create a middleware that handles routing for /inventory */
